442459463906306406b4a82fc47bbe700b4b5c8a
SuhailLoya <suhail.loya@gmail.com>
2024-01-25 20:14:15 +0800

    minor changes

--- Backend/config/initializers/cors.rb
+++ Backend/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "http://localhost:5173", "https://chatter-cube-cvwo-project-1trf.vercel.app"
+    
 
     resource "*",
       headers: :any,
